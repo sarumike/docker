aea16659e873636ddcef16894ce90158c66d1042
Mike Jenner <sarumike@users.noreply.github.com>
2020-07-10 14:45:00 +0100

    Add files via upload

---
+++ dev_ubuntu_18.04/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --no-cache -t ubuntu:18.04 .
---
+++ dev_ubuntu_18.04/run.sh
@@ -0,0 +1,4 @@
+#sudo docker run --privileged -it --rm dev_ubuntu:18.04 /bin/bash
+
+docker run      --mount type=bind,src=/home/mike/test,dst=/root/sv \
+	                --name vc_sv -it --net host txblaster /bin/bash
