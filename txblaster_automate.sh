e497894801fab74f0aabe4006f03af4731ec04a5
Mike Jenner <m.jenner@nchain.com>
2020-11-18 09:41:28 +0000

    updated with variables

--- txblaster_automate.sh
+++ txblaster_automate.sh
@@ -1,56 +1,76 @@
 #!/bin/bash
 
+#define conf file 
+config=~/RegTests/Data/txbtests/regtest.conf
+
+#define regtest folder
+regtest=~/RegTests/Data/txbtests/output
+
+#define output folder to hold txns generated
+output=~/RegTests/Data/txbtests/output
+
+#define private keys location
+keys=~/RegTests/Data/524K_privateKeysWithPublic.txt
+
+#define bsv binaries location
+bsv=~/bitcoin-sv-1.0.5_difficulty/src
+
+#set variables for blocks and coins to send
+blocks=20000
+coins=14949
+
+
 echo "############ read values from .conf file ###################"
 echo ""
 
-port=$(awk '/^port=/ {print substr($1,6)}' ~/.bitcoin/bitcoin.conf)
-rpcport=$(awk '/^rpcport=/ {print substr($1,9)}' ~/.bitcoin/bitcoin.conf)
-rpcuser=$(awk '/^rpcuser=/ {print substr($1,9)}' ~/.bitcoin/bitcoin.conf)
-rpcpwd=$(awk '/^rpcpassword=/ {print substr($1,13)}' ~/.bitcoin/bitcoin.conf)
+port=$(awk '/^port=/ {print substr($1,6)}' $config)
+rpcport=$(awk '/^rpcport=/ {print substr($1,9)}' $config)
+rpcuser=$(awk '/^rpcuser=/ {print substr($1,9)}' $config)
+rpcpwd=$(awk '/^rpcpassword=/ {print substr($1,13)}' $config)
 
 
 echo "Step 1: stop node if running"
-~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf stop
+$bsv/bitcoin-cli -conf=$config stop
 
-sleep 10
+sleep 60
 
 echo "Step 2: remove regtest folder"
 
-rm -r ~/.bitcoin/regtest
+rm -r $regtest/regtest
 
 sleep 10
 
 echo "Step 3: start up bsv node"
 
-~/sv/src/bitcoind -genesisactivationheight=100 -maxstdtxvalidationduration=1000 -maxnonstdtxvalidationduration=5000 -maxscriptsizepolicy=0 -excessiveblocksize=0 -maxstackmemoryusageconsensus=0  -daemon
+$bsv/bitcoind -conf=$config -genesisactivationheight=100 -maxstdtxvalidationduration=3000 -maxnonstdtxvalidationduration=5000 -maxscriptsizepolicy=0 -excessiveblocksize=0 -maxstackmemoryusageconsensus=0  -limitancestorcount=500 -limitdescendantcount=500 -daemon
 sleep 30
 
 echo "Step 4: generate empty blocks"
-~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf generate 200
+$bsv/bitcoin-cli -conf=$config generate $blocks
 sleep 10
 
 echo "Step 5: grab new address"
-newaddr=$(~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf getnewaddress)
+newaddr=$($bsv/bitcoin-cli -conf=$config getnewaddress)
 
 echo "new address is  " $newaddr
 
 sleep 10
 
 echo "Step 6: dump out private key from address"
-dumpkey=$(~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf dumpprivkey $newaddr)
+dumpkey=$($bsv/bitcoin-cli -conf=$config dumpprivkey $newaddr)
 
 echo "private key is " $dumpkey
 
 sleep 10
 
 echo "Step 7: send coins to address"
-addr=$(~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf sendtoaddress $newaddr 200) 
+addr=$($bsv/bitcoin-cli -conf=$config sendtoaddress $newaddr $coins) 
 sleep 10
 
 #echo "txn is " $addr
 
 echo "Step 8: get raw txn for funding"
-rawtxn=$(~/sv/src/bitcoin-cli -conf=/home/mike/.bitcoin/bitcoin.conf getrawtransaction $addr) 
+rawtxn=$($bsv/bitcoin-cli -conf=$config getrawtransaction $addr) 
 
 echo "raw txn is " $rawtxn
 
@@ -59,12 +79,13 @@ echo "Step 9: create json file for txblaster"
 
 echo "{" >> generate.json
 echo "\""networkType\"": \""RegTest\"", " >> generate.json
-echo "\""savePathRoot\"": \""/root/sv\""," >> generate.json
+echo "\""savePathRoot\"": \""$output\""," >> generate.json
 echo "\""fundingPrivateKeyWIF\"": \""$dumpkey\"", " >> generate.json
 echo "\""fundingTransaction\"": \""$rawtxn\"", " >> generate.json
 echo "\""numberOfInputsPerTransaction\"": 1, " >> generate.json
 echo "\""numberOfOutputsPerTransaction\"": 2, " >> generate.json
-echo "\""numberOfTransactionsToGenerate\"": 100000, " >> generate.json
+echo "\""numberOfTransactionsToGenerate\"": 1000000, " >> generate.json
+echo "\""maxChainLength\"": 20, " >> generate.json 
 echo "" >> generate.json
 echo "\""verifyRpcNodeParameters\"": {" >> generate.json
 echo "          \""rpcAddress\"":  \""127.0.0.1:$rpcport\""," >> generate.json
@@ -81,9 +102,9 @@ echo "		}" >> generate.json
 echo "	]," >> generate.json
 echo "" >> generate.json
 echo "\""submitLastBatch\"": false, " >> generate.json
-echo "\""sessionName\"": \""100k_txns\"", " >> generate.json
+echo "\""sessionName\"": \""difficulty_1M_txns\"", " >> generate.json
 echo "\""logToConsole\"": true, " >> generate.json 
-echo "\""privateKeysFile\"": \""524K_privateKeysWithPublic.txt\"" " >> generate.json
+echo "\""privateKeysFile\"": \""$keys\"" " >> generate.json
 echo "" >> generate.json
 echo "}" >> generate.json
 
