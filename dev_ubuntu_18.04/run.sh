e11675cbd6ae4d1f006babeaccf83341f7344b91
Mike Jenner <m.jenner@nchain.com>
2020-11-18 09:23:57 +0000

    updaet build.sh with correct target

--- dev_ubuntu_18.04/run.sh
+++ dev_ubuntu_18.04/run.sh
@@ -1,4 +1,4 @@
 #sudo docker run --privileged -it --rm dev_ubuntu:18.04 /bin/bash
 
-docker run      --mount type=bind,src=/home/mike/test,dst=/root/sv \
-	                --name vc_sv -it --net host txblaster /bin/bash
+docker run      --mount type=bind,src=/home/mike/txb_files,dst=/root/sv \
+	                --name vc_sv -it --network host txblaster /bin/bash
